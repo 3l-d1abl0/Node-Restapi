743e8f8f5c77e1c83b2e4ef5fe00781de071f860
Sameer Barha <sameer.barha12@gmail.com>
2018-06-24 21:31:58 +0530

    Setup Order and products Route

---
+++ api/routes/products.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/',(req, res, next)=>{
+  res.status(200).json({
+    message :  "Handling GET on /products"
+  });
+});
+
+router.post('/',(req, res, next)=>{
+  res.status(201).json({
+    message :  "Handling POST on /products"
+  });
+});
+
+router.get('/:productId',(req, res, next)=>{
+  const id = req.params.productId;
+
+  if(id>0){
+    res.status(200).json({
+      message :  "GET with ID",
+      id: id
+    });
+  }else{
+    res.status(200).json({
+      message : "Something is Wrong with this Id",
+      id: id
+    });
+  }
+});
+
+router.patch('/:productId',(req, res, next)=>{
+    res.status(201).json({
+      message :  "Product Updated !"
+    });
+});
+
+router.delete('/:productId',(req, res, next)=>{
+    res.status(201).json({
+      message :  "Product Deleted !"
+    });
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express  = require('express');
 const app = express();
 
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+
 app.use((req, res, next) =>{
+  console.log(req.url);
   res.status(200).json({
     message: "Working !"
   });
