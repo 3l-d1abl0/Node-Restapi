d5d9fa0e5c59d1d21dc55f4dbcf2a75cef0ce98e
sameerb <sameer.barha12@gmail.com>
2018-07-01 15:28:03 +0530

    added mongoose and formatted response

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,28 +1,86 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+//import Models
+const Product = require('../models/product');
 
 router.get('/',(req, res, next)=>{
-  res.status(200).json({
-    message :  "Handling GET on /products"
-  });
+
+  Product.find()
+    .exec()
+    .then(docs=>{
+      console.log(docs);
+      console.log(docs.length);
+
+      if(docs.length >0){
+        res.status(200).json(docs);
+      }else{
+        res.status(404).json({
+          message: "No entries Found !"
+        });
+      }
+
+
+    })
+    .catch(err=>{
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+
 });
 
 router.post('/',(req, res, next)=>{
-
+  /*
   const product = {
       name : req.body.name,
       price: req.body.price
-  };
+  };*/
 
-  res.status(201).json({
-    message :  "Handling POST on /products",
-    createdProduct: product
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    price: req.body.price
   });
+  product
+    .save()
+    .then(result=>{
+        console.log(result);
+        res.status(201).json({
+          message :  "Handling POST on /products",
+          createdProduct: product
+        });
+     })
+   .catch(err=> {
+     console.log(err);
+     res.status(500).json({
+       error: err
+     });
+   });
+
+
 });
 
 router.get('/:productId',(req, res, next)=>{
   const id = req.params.productId;
+  Product.findById(id)
+    .exec()
+    .then(doc => {
+      console.log(doc);
+      if(doc){
+        res.status(200).json(doc);
+      }else{
+        res.status(404).json({message: "No valid Entry !"});
+      }
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json({error: err});
+    });
 
+  /*
   if(id>0){
     res.status(200).json({
       message :  "GET with ID",
@@ -33,19 +91,60 @@ router.get('/:productId',(req, res, next)=>{
       message : "Something is Wrong with this Id",
       id: id
     });
-  }
+  } */
 });
 
+//Update a Product
 router.patch('/:productId',(req, res, next)=>{
-    res.status(201).json({
+    const id = req.params.productId;
+
+    const updateOps = {};
+    for(const ops of req.body){
+        updateOps[ops.propName] = ops.value;
+    }
+
+    Product.update({ _id: id}, {$set: updateOps })
+      .exec()
+      .then(result=>{
+        console.log(result);
+        res.status(200).json(result);
+      })
+      .catch(err=>{
+        console.log(err);
+        res.status(500).json({
+          error: err
+        });
+      });
+    //Product.update({ _id: id}, {$set: {name: req.body.newName, price: req.body.newPrice} });
+
+    /*res.status(201).json({
       message :  "Product Updated !"
-    });
+    });*/
 });
 
+
+
+//Handles Delete request
 router.delete('/:productId',(req, res, next)=>{
+
+    const id = req.params.productId;
+    Product.remove({_id: id})
+    .exec()
+    .then(result=>{
+      res.status(200).json(result);
+    })
+    .catch(err=>{
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+
+    /*
     res.status(201).json({
       message :  "Product Deleted !"
     });
+    */
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express  = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 app.use((req, res, next)=>{
   res.header('Access-Control-Allow-Origin','*');
@@ -18,6 +19,15 @@ app.use((req, res, next)=>{
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb://localhost/rest-api',function(err){
+  if(err){
+    console.log(err);
+  }else{
+    console.log('Connected to MongoDB!');
+  }
+});
+
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
